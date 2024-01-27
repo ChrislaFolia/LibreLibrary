dcab2d157a15bd7a9eed2a5760779e75a0acf59f
Chrislafolia <frank08ta@gmail.com>
2024-01-27 14:27:01 +0800

    Establish DAO for 4 following tables, Book, BorrowingRecord, Inventory and LibUser

---
+++ LibreLibrary-server/librelibrary/src/main/java/com/liblib/book/model/InventoryRepository.java
@@ -0,0 +1,7 @@
+package com.liblib.book.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Integer>  {
+
+}
---
+++ LibreLibrary-server/librelibrary/src/main/java/com/liblib/user/model/LibUserRepository.java
@@ -0,0 +1,7 @@
+package com.liblib.user.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibUserRepository  extends JpaRepository<LibUser, Integer> {
+
+}
