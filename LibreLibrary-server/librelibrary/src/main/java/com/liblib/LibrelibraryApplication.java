108e9535dd3e020dbfe5629053d2a57a560da63c
Chrislafolia <frank08ta@gmail.com>
2024-01-27 14:15:42 +0800

    Start a LibreLibrary backend server Spring Boot project

---
+++ LibreLibrary-server/librelibrary/src/main/java/com/liblib/LibrelibraryApplication.java
@@ -0,0 +1,13 @@
+package com.liblib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrelibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrelibraryApplication.class, args);
+	}
+
+}
